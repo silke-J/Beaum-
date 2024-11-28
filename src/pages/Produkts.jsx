d8d2e9a896c0a6cdae2a05865e04c39ee0f9fdb7
silke-J <silk0776@mcdmstudent.dk>
2024-11-28 12:45:12 +0100

    Beaumé

--- src/pages/Produkts.jsx
+++ src/pages/Produkts.jsx
@@ -29,7 +29,7 @@ const Products = () => {
 
   return (
     <section>
-      <PageHeader img={img}  />
+      <PageHeader img={img} title="Produkter"  />
       <div>
         <Button
           buttonText="All"
